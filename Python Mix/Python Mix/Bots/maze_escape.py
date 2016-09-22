d47b6a3ca670958aa4ed4d86c8d13cb6af6ae408
matbrik123 <matbrik@gmail.com>
2016-09-22 17:57:08 +0200

    ctf

--- Python Mix/Python Mix/Bots/maze_escape.py
+++ Python Mix/Python Mix/Bots/maze_escape.py
@@ -7,16 +7,52 @@ e--#--#
 #######
 
 """
-def next_move(board):
-    for i in range(3):
-        for j in range(3):
-            if board[i][j]=='e':
-                if i==0 and j==1:
-                        
+import os
 
+
+def next_move(lastMove,board):
+    if lastMove==0:
+        if board[0][1]=='#' and board[1][2]=='#':     
+            print("DOWN")
+            return
+        if board[0][1]=='-' and board[1][2]=='-':     
+            print("UP")
+            return
+        if board[0][1]=='#' and board[1][2]=='-':     
+            print("LEFT")
+            return
+        if board[0][1]=='-' and board[1][2]=='#':     
+            print("RIGHT")
+            return
+
+
+    if lastMove in [1,2,4,5,6,7,8]:
+        print("UP") 
+        return  
+    if lastMove in [3]:
+        print("RIGHT")   
+        return                 
+
+def init():
+    lastMove=0
+    filename="test.txt"
+    if not os.path.isfile('./test.txt'):
+        with open( filename, "w") as f:
+            f.write("1")
+    else:
+        with open( filename ) as f:
+        # file read can happen here
+        # print "file exists"
+            lastMove=f.readline()
+            lastMove=int(lastMove.strip())
+        with open( filename, "w") as f:
+            f.write(str(lastMove+1))
+    return lastMove
 
 
 if __name__ == "__main__": 
     pos = [int(i) for i in input().strip().split()] 
     board = [[j for j in input().strip()] for i in range(3)]  
-    next_move(board)
+
+    lastMove=init()
+    next_move(lastMove,board)
