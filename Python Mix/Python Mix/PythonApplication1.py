3c26643f6d4137e156d2ba423e0f2112b970d710
matbrik123 <matbrik@gmail.com>
2016-09-22 18:05:28 +0200

    ctf2

--- Python Mix/Python Mix/PythonApplication1.py
+++ Python Mix/Python Mix/PythonApplication1.py
@@ -3,38 +3,75 @@ import hashlib
 import itertools as it
 import string
 import socket
+from brainfuck import Nice
 
+
+s = socket.socket()
+s.connect(("lse.epita.fr", 52108))
+data=s.recv(1024)+s.recv(1024)  
+data=data.strip().split('\n')
+print(data)
+nice=Nice.NiceInterpreter()
+nice.execute(data[1])
+test=nice.array
+print(test[2:7])
+sum=0
+for i in test[2:7]:
+    sum*=10
+    sum+=i-48
+
+print(sum)
+
+s = socket.socket()
+s.connect(("lse.epita.fr", sum))
+s.send(data[0].split(": ")[1])
+rec=s.recv(1024)
+print(rec)
+
+nice=Nice.NiceInterpreter()
+nice.execute(rec)
+test=nice.array
+print(test[2:7])
+sum=0
+for i in test[2:7]:
+    sum*=10
+    sum+=i-48
+
+print(sum)
+
+s = socket.socket()
+s.connect(("lse.epita.fr", sum))
+s.send(data[0].split(": ")[1])
+rec=s.recv(1024)
+print(rec)
+
+while True:
+    nice=Nice.NiceInterpreter()
+    nice.execute(rec)
+    test=nice.array
+    print(test[2:7])
+    sum=0
+    for i in test[2:7]:
+        sum*=10
+        sum+=i-48
     
+    print(sum)
+    
+    s = socket.socket()
+    s.connect(("lse.epita.fr", sum))
+    s.send(data[0].split(": ")[1])
+    rec=s.recv(1024)
+    print(rec)
 
-stringtmp="1234567890qwertyuiopasdfghjklzxcvbnm<>,.;:-_òàù+è§°çé*#@[]{}ì'^?=)(/&%$£\"!|"
-i=0
-tmp=""
-lista=[42001,
-42109,
-42315,
-42316,
-42326,
-42327,
-42329,
-42338,
-42350,
-42358,
-42368,
-42370,
-42373,
-42382,
-42383,
-42386,
-42387,
-42389,
-42391,
-42394
-]
-lista2=[]
+
+
+
+"""   
+
+
 for addr in lista:
     s = socket.socket()
-
-    s.connect(("lse.epita.fr", addr))
+    s.connect(("lse.epita.fr", 51108))
     c=0
     try:
         s.recv(5)
@@ -43,4 +80,4 @@ for addr in lista:
         s.close()
     print(addr)
     # stage 1: works
-
+"""
