b38fc7a8f65865ad8e91088d709e43fb07afee04
matbrik123 <matbrik@gmail.com>
2017-03-20 14:08:49 +0100

    dd

--- Python Mix/Python Mix/mng/mainpart.py
+++ Python Mix/Python Mix/mng/mainpart.py
@@ -7,6 +7,8 @@
 5 aspetto n minuti 
 6 punto 1
 """
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 import os
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
